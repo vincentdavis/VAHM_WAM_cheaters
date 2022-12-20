b356f119f7e7a76e15a63f4246e1ed1db51f13ab
Vincent Davis <vincent@heteroskedastic.net>
2022-12-20 12:07:59 -0700

    Export to excel example

--- loadfiles.py
+++ loadfiles.py
@@ -1,9 +1,11 @@
+import os
 from datetime import datetime, timedelta
 from typing import Dict, Union, Optional
-import os
 
 import fitdecode
 import pandas as pd
+
+pd.set_option('mode.chained_assignment', None)
 
 
 def load_fit(fit_file: str) -> pd.DataFrame:
@@ -12,6 +14,110 @@ def load_fit(fit_file: str) -> pd.DataFrame:
     print(f"Loading: {fit_file}")
     df_lap, df = conv.fit_to_dataframes(fname=fit_file)
     return df
+
+
+def slots2dict(classobj: object) -> dict:
+    "Convert a class object with slots to a dict"
+    {s: getattr(classobj, s, None) for s in classobj.__slots__}
+    return {s: getattr(classobj, s, None) for s in classobj.__slots__}
+
+
+def FieldDefinition2dict(frame: fitdecode.records.FitDefinitionMessage) -> dict:
+    "Convert a FieldDefinition to dict"
+    frame_dict = {}
+    for d in frame.field_defs:
+        frame_dict['name'] = d.name
+        frame_dict['is_dev'] = d.is_dev
+        frame_dict['type']: d.type.name
+    return frame_dict
+
+    # return {s: getattr(FieldDefinition, s, None) for s in FieldDefinition.__slots__}
+
+
+def frame2dict(frame: fitdecode.records.FitDataMessage) -> dict:
+    "Convert a fonvert frame fields to dict"
+    frame_dict = {}
+    for field in frame.fields:
+        # print(f"Frame Field Name: {field.value}")
+        try:
+            frame_dict[field.name] = frame.get_value(field.name)
+        except KeyError:
+            frame_dict[field.name] = None
+    return frame_dict
+
+
+def fit2dicts(fit_file: str) -> tuple[
+    list[dict, ...], list[dict, ...], list[dict, ...], list[dict, ...], list[dict, ...]]:
+    """Load a fit file"""
+    input_extension = os.path.splitext(fit_file)[1]
+    if input_extension.lower() != '.fit':
+        raise fitdecode.exceptions.FitHeaderError("Input file must be a .FIT file.")
+    FitHeaders = []
+    FitDefinitionMessages = []
+    FitDataMessages = []
+    FitCRCs = []
+    fitother = []
+    with open(fit_file, 'rb') as fitfile:
+        fit = fitdecode.FitReader(fitfile)
+        for i, frame in enumerate(fit):
+            match frame:
+                case fitdecode.records.FitHeader():
+                    FitHeaders.append(slots2dict(frame))
+                case fitdecode.records.FitDefinitionMessage():
+                    FitDefinitionMessages.append(FieldDefinition2dict(frame))
+                    # FitDefinitionMessages.append({i: slots2dict(frame)})
+                case fitdecode.records.FitDataMessage():
+                    FitDataMessages.append(frame2dict(frame))
+                    # FitDataMessages.append({i: slots2dict(frame)})
+                case fitdecode.records.FitCRC():
+                    FitCRCs.append(slots2dict(frame))
+                case _:
+                    fitother.append(slots2dict(frame))
+    return FitHeaders, FitDefinitionMessages, FitDataMessages, FitCRCs, fitother
+
+
+def fit2df(fit_file: str) -> tuple[pd.DataFrame, ...]:
+    """Load a fit file into a pandas dataframe"""
+    FitHeaders, FitDefinitionMessages, FitDataMessages, FitCRCs, fitother = fit2dicts(fit_file)
+    FitHeaders_df = pd.DataFrame(FitHeaders)
+    FitDefinitionMessages_df = pd.DataFrame(FitDefinitionMessages)
+    FitDataMessages_df = pd.DataFrame(FitDataMessages)
+    FitDataMessages_df.dropna(how='all', axis='columns', inplace=True)
+    Data = FitDataMessages_df[(FitDataMessages_df.event_type == 'marker') | (FitDataMessages_df.distance.notnull())]
+    Data.dropna(how='all', axis='columns', inplace=True)
+    file_data = FitDataMessages_df[
+        (FitDataMessages_df['distance'].isnull()) & (FitDataMessages_df['event_type'] != 'marker')]
+    file_data.dropna(how='all', axis='columns', inplace=True)
+    file_data.dropna(how='all', axis='rows', inplace=True)
+    FitCRCs_df = pd.DataFrame(FitCRCs)
+    fitother_df = pd.DataFrame(fitother)
+    return FitHeaders_df, FitDefinitionMessages_df, file_data, Data, FitCRCs_df, fitother_df
+
+
+def fit2excel(fit_file: str, excel_file: str, remove_unknown=True) -> None:
+    """Load a fit file into a pandas dataframe"""
+    FitHeaders_df, FitDefinitionMessages_df, file_data, Data, FitCRCs_df, fitother_df = fit2df(fit_file)
+    with pd.ExcelWriter(excel_file) as writer:
+        FitHeaders_df.to_excel(writer, sheet_name='FitHeaders')
+        FitDefinitionMessages_df.to_excel(writer, sheet_name='FitDefinitionMessages')
+        if remove_unknown:
+            file_data = file_data[[c for c in file_data if 'unknown' not in c]]
+            Data = Data[[c for c in Data if 'unknown' not in c]]
+
+        # deal with timezone columns in FitDataMessages_df
+        for d in [file_data, Data]:
+            date_columns = d.select_dtypes(include=['datetime64[ns, UTC]']).columns
+            for c in date_columns:
+                try:
+                    d[c] = d[c].apply(
+                        lambda a: datetime.strftime(a, "%Y-%m-%d %H:%M:%S") if not pd.isnull(a) else '')
+                except:
+                    raise
+        file_data.to_excel(writer, sheet_name='file_data')
+        Data.to_excel(writer, sheet_name='Data')
+
+        FitCRCs_df.to_excel(writer, sheet_name='FitCRCs')
+        fitother_df.to_excel(writer, sheet_name='fitother')
 
 
 class Converter:
